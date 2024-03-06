bca162e98c359bee568d47100e8804fc010cca9c
H <151752850+IamHkoch@users.noreply.github.com>
2024-03-06 13:33:17 +0000

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,33 @@
+##simple interactive program
+
+##use .lower() to make input case insensitive
+
+print('Find out your predictions for 2024!')
+zodiacsign = str(input('Please enter your zodiac sign: ')).lower()
+
+if zodiacsign == 'libra':
+    print('"2024 features two intense eclipses in your sign, which may stir feelings of anxiety and require extra self-care. However, there is a silver lining: Venus, your ruling planet, remains direct all year", (Allure.com).')
+elif zodiacsign == 'cancer' :
+    print('"Pluto moves into your 8th House of Sex, inviting you to explore new realms of sensuality, whether that means pausing dating, embracing sexual fluidity, or delving into kink and fantasy", (Allure.com).')
+elif zodiacsign == 'capricorn' :
+    print('"Expansive Jupiter enters your 6th House of Health, fueling your resolve to conquer fears or vices. Harness your signature strength, sea-goat, to overcome obstacles in your way of growth", (Allure.com).')
+elif zodiacsign == 'taurus' :
+    print('"Uranus introduces elements of surprise and unpredictability, which can be challenging for your earthy, stable nature. Expect the unexpected, particularly as Uranus undergoes two retrogrades", (Allure.com).')
+elif zodiacsign == 'gemini' :
+    print('"2024 promises to be a significant change for you, but autumn is the most transformative time. These months mark the realization of goals you have been striving towards during the earlier part of the year", (Allure.com).')
+elif zodiacsign == 'aries' :
+    print('"In 2024, you will experience a broadening of your social circle and a refreshing transformation in your professional and creative endeavors. As Mars, your ruling planet, enters retrograde, be prepared for its impact on your sex life and career", (Allure.com).')
+elif zodiacsign == 'virgo' :
+    print('"In 2024, your confidence will reach new heights. The year starts with Pluto moving into your 6th House of Health, a transition ideal for revitalizing your wellness and beauty routines", (Allure.com).')
+elif zodiacsign == 'scorpio' :
+    print('"2024 is a year guided by introspection and transformative shifts, influenced by the retrogrades of both of your ruling planets, Mars and Pluto. Pluto retrograde emphasizes the importance of self-care, urging you to focus on nurturing yourself as a critical aspect of your well-being", (Allure.com).')
+elif zodiacsign == 'leo' :
+    print('"Your 2024 horoscope is a journey through professional and personal transformations. The year kicks off with Uranus retrograde influencing your 10th House of Social Status, a phase that is crucial for reshaping your professional image", (Allure.com).')
+elif zodiacsign == 'sagittarius' :
+    print('"Spring ushers in a romantic period, heightened during Gemini season with its flirty floral essence. However, the year also suggests stress with two eclipses that potentially disrupt friendships with sudden changes and heightened emotions", (Allure.com).')
+elif zodiacsign == 'aquarius' :
+    print('"Your 2024 horoscope centers on finding equilibrium in your life, from deepening personal relationships to advancing your career and prioritizing self-care", (Allure.com).')
+elif zodiacsign == 'pisces':
+    print('"Prepare for a year marked by transformative eclipses and a crucial retrograde of your ruling planet, Neptune, emphasizing the need for self-care. The years first major lunar eclipse affects your 8th House of Sex, bringing rapid changes in your intimate life, especially in areas you have been hesitant to address", (Allure.com).')
+else :
+    print('Oops, that star sign does not exist. Try again!')
